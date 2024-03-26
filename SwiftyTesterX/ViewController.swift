9ba59d84d54694fd0d67fd5d554ba4a252c6aced
Ильдар <arslambekov63@gmail.com>
2024-03-26 14:23:49 +0300

    Initial Commit

---
+++ SwiftyTesterX/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftyTesterX
+//
+//  Created by Ильдар Арсламбеков on 26.03.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
